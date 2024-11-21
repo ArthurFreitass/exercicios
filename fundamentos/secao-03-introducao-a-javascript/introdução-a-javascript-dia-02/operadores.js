09979f1e1a494aba7cb6dcdffc0a6582a295c13a
Arthur <freitarthur176@gmail.com>
2024-11-21 09:38:52 -0300

    operator directory

---
+++ fundamentos/secao-03-introducao-a-javascript/introdução-a-javascript-dia-02/operadores.js
@@ -0,0 +1,23 @@
+let sum = (5 + 4)
+console.log(sum);
+
+let subtraction = 6;
+
+console.log(sum -= subtraction);
+
+// Soma
+
+console.log(5 + 32);
+// Subtração
+
+console.log (4 - 4);
+
+// Divisão
+
+console.log(3 / 3);
+
+// Resto
+
+console.log(2 % 2);
+
+
