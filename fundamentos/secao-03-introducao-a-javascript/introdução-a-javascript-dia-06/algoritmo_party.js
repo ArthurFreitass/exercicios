df6f5a4d54cd271f560b397ed28480751f79ce7b
Arthur <freitarthur176@gmail.com>
2024-12-19 10:20:30 -0300

    Add day 6

---
+++ fundamentos/secao-03-introducao-a-javascript/introdução-a-javascript-dia-06/algoritmo_party.js
@@ -0,0 +1,13 @@
+// Algoritmo para ir a uma festa
+
+let party = false;
+
+if (party == true) {
+    let getReady = true;
+    let roupa = "Vou com minha melhor roupa e no molho!";
+    console.log(roupa);
+
+} else if (party == false) {
+    console.log("Vou ficar em casa mesmo. :(");
+
+}
