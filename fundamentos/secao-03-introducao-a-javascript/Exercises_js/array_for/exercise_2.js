135fc14eac80055b9f242ebed08b5604d691ebd9
Arthur <freitarthur176@gmail.com>
2024-12-20 16:30:20 -0300

    Completed exercise two

---
+++ fundamentos/secao-03-introducao-a-javascript/Exercises_js/array_for/exercise_2.js
@@ -0,0 +1,15 @@
+// Área para declaração de variáveis
+
+let sum = 1;
+let factorial = 1;
+let factorial_1 = 0;
+
+// Loop de repetição
+
+for (let index = 1; index <= 10; index += 1) {
+  sum = sum * index;
+}
+
+// Saída
+
+console.log(`O fatorial de 10 é ${sum}`);
