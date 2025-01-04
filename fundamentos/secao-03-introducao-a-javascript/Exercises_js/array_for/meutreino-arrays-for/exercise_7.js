eb23ab0946131b074a4ef34f0e299d8041387299
Arthur <freitarthur176@gmail.com>
2025-01-04 17:12:07 -0300

    ADD exercises medium difficulty

--- fundamentos/secao-03-introducao-a-javascript/Exercises_js/array_for/meutreino-arrays-for/exercise_7.js
+++ fundamentos/secao-03-introducao-a-javascript/Exercises_js/array_for/meutreino-arrays-for/exercise_7.js
@@ -2,7 +2,7 @@ const numbers_Array = [-10, 131, 17, 19, 1455];
 let sum = 0;
 
 for (let index = 0; index < numbers_Array.length; index += 1) {
-  sum += numbers_Array[index] + sum;
+  sum += numbers_Array[index];
 }
 
 console.log(sum);
---
+++ fundamentos/secao-03-introducao-a-javascript/Exercises_js/array_for/meutreino-arrays-for/exercise_medium_2.js
@@ -0,0 +1,26 @@
+// Área para a declaração de variáveis
+
+const message = "Vamos viver e cantar";
+
+let message_Array = message.split("");
+
+let vowel_Counter = 0;
+
+// Loop e lógica
+
+for (let index = 0; index < message.length; index += 1) {
+  if (message_Array[index] == "a") {
+    vowel_Counter += 1;
+  } else if (message_Array[index] == "e") {
+    vowel_Counter += 1;
+  } else if (message_Array[index] == "i") {
+    vowel_Counter += 1;
+  } else if (message_Array[index] == "o") {
+    vowel_Counter += 1;
+  } else if (message_Array[index] == "u") {
+    vowel_Counter += 1;
+  }
+}
+// Saída
+
+console.log(`O número de vogais é ${vowel_Counter}`);
