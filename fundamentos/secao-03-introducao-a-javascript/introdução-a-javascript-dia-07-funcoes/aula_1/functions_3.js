c9d5029a718aea108d6c373304410af2479ec298
Arthur <freitarthur176@gmail.com>
2025-01-22 08:58:23 -0300

    Add aula 3

--- fundamentos/secao-03-introducao-a-javascript/introdução-a-javascript-dia-07-funcoes/aula_1/functions_3.js
+++ fundamentos/secao-03-introducao-a-javascript/introdução-a-javascript-dia-07-funcoes/aula_1/functions_3.js
@@ -1,16 +1,25 @@
 const trybeBankCustomers = ["Oliva", "Nat", "Gus"];
 
-function addCustomers(costumers, newCostumers) {
-  for (let index = 0; index < newCostumers.length; index += 1) {
-    if (typeof newCostumers[index] === "string") {
-      costumers.push(newCostumers[index]);
+// customer está recebendo trybebank - o outro parâmetro irá receber os novos clientes
+
+function addCustomers(customer, novosClientes) {
+  for (let index = 0; index < customer.length; index += 1) {
+    if (typeof customer[index] != "string") {
+      return "O parâmetro passado deve ser do tipo string";
+    }
+  }
+  // Loop verifica se cada valor no parâmetro customer é uma string
+
+  for (let index = 0; index < novosClientes.length; index += 1) {
+    if (typeof novosClientes[index] === "string") {
+      customer.push(novosClientes[index]);
     } else {
       return "Todos os valores precisam ser strings";
     }
   }
-  return trybeBankCustomers;
+  return "Pessoa adicionada com sucesso!";
+
+  // loop verifica se cada elemento é uma string no novo parâmetro, e se for adiciona ao final
 }
 
-console.log(addCustomers(trybeBankCustomers, ["Joaquim"]));
-
-// Pessoa adicionada com sucesso!
+console.log(addCustomers(trybeBankCustomers, ["Evaristo"])); // Pessoa adicionada com sucesso!
